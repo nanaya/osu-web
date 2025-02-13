059d16bffab04420200d47461c8b5e4e38260dfb
nanaya <me@nanaya.net>
2025-02-13 20:15:02 +0900

    Add typing for webpack require

--- tests/karma/index.ts
+++ tests/karma/index.ts
@@ -8,10 +8,5 @@ import '../../resources/builds/locales/en';
 // Doesn't work when specified in karma config for some reason.
 import '../../resources/js/entrypoints/app';
 
-// webpack's require typings are different from node's
-// and installing either of those typings breaks the typings for the web stuff
-// because the typings for global functions like setTimeout are different.
-declare let require: any;
-
 const testsContext = require.context('.', true, /\.spec$/);
 testsContext.keys().forEach(testsContext);
